7d02196d804bac9f1735c86238b6dd0bf2a81f8e
wxzle7 <leticia.carvalho156@etec.sp.gov.br>
2024-02-22 10:40:41 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Letícia Lopes de Carvalho</Text>
+      <StatusBar style="auto" />
+       <Text>DDM</Text>
+      <StatusBar style="auto" />
+    </View>
+  );}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
